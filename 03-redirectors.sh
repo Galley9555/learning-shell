5ab869db8b42f74a042a126438bfecf4daffe929
OM SAI RAM JI <ricxrocxx1988@gmail.com>
2023-08-26 11:25:53 +0530

    Adding redirectors on Frontend Server

---
+++ 03-redirectors.sh
@@ -0,0 +1,18 @@
+echo Installing Nginx
+yum install nginx -y &>>/tmp/frontend
+
+echo Downloading Nginx Web Content
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>/tmp/frontend
+
+cd /usr/share/nginx/html
+rm -rf * &>>/tmp/frontend
+
+echo Extracting Web Content
+unzip /tmp/frontend.zip &>>/tmp/frontend
+
+mv frontend-main/static/* . &>>/tmp/frontend
+mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/frontend
+
+echo Starting Nginx Sevice
+systemctl enable nginx &>>/tmp/frontend
+systemctl restart nginx &>>/tmp/frontend
