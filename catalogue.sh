9ae748194d03c3846c44185b90d163d0a6d2456b
OM SAI RAM JI <ricxrocxx1988@gmail.com>
2023-08-28 13:09:21 +0530

    Creating catologue Server

--- catalogue.sh
+++ catalogue.sh
@@ -2,36 +2,71 @@ LOG_FILE=/tmp/catalogue
 
 echo "Setup NodeJS Repos"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo Status = Success
+else
+  echo Status = Failure
+  exit
+  fi
 
 echo "Install NodeJS"
 yum install nodejs -y &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo Status = Success
+else
+  echo Status = Failure
+  exit
+  fi
 
 echo "Adding Roboshop Application User"
 useradd roboshop &>>${LOG_FILE}
-echo status =$?
+if [ $? -eq 0 ]; then
+  echo Status = Success
+else
+  echo Status = Failure
+  exit
+  fi
 
 echo "Downloading Catalogue Application Code"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo Status = Success
+else
+  echo Status = Failure
+  exit
+  fi
 
 cd /home/roboshop
 
 echo "Extracting Catalogue Application Code"
 unzip /tmp/catalogue.zip &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo Status = Success
+else
+  echo Status = Failure
+  exit
+  fi
 
 mv catalogue-main catalogue
 cd /home/roboshop/catalogue
 
 echo "Install NodeJS Dependencies"
 npm install &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo Status = Success
+else
+  echo Status = Failure
+  exit
+  fi
 
 echo "Setup catalogue Service"
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo Status = Success
+else
+  echo Status = Failure
+  exit
+  fi
 
 
 systemctl daemon-reload &>>${LOG_FILE}
@@ -40,4 +75,9 @@ systemctl enable catalogue &>>${LOG_FILE}
 
 echo "Start Cataloge Service"
 systemctl start catalogue &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo Status = Success
+else
+  echo Status = Failure
+  exit
+  fi
