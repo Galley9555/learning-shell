ca54ad8cf9ca181777198fe3678c15ff215ff36c
OM SAI RAM JI <ricxrocxx1988@gmail.com>
2023-11-08 12:38:25 +0530

    Catalogue Server Function added

--- catalogue.sh
+++ catalogue.sh
@@ -6,85 +6,52 @@ if [ $ID -ne 0 ]; then
   exit 1
 fi
 
-echo "Setup NodeJS Repos"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG_FILE}
-if [ $? -eq 0 ]; then
+StatusCheck() {
+if [ $1 -eq 0 ]; then
   echo Status = Success
 else
   echo Status = Failure
   exit 1
 fi
+echo "Setup NodeJS Repos"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG_FILE}
+StatusCheck $?
+
 
 echo "Install NodeJS"
 yum install nodejs -y &>>${LOG_FILE}
-if [ $? -eq 0 ]; then
-  echo Status = Success
-else
-  echo Status = Failure
-  exit 1
-fi
+StatusCheck $?
 
 id roboshop &>>${LOG_FILE}
 if [ $? -ne 0 ]; then
  echo "Adding Roboshop Application User"
  useradd roboshop &>>${LOG_FILE}
- if [ $? -eq 0 ]; then
-  echo Status = Success
-else
-  echo Status = Failure
-  exit 1
-  fi
-fi
+ StatusCheck $?
 
 echo "Downloading Catalogue Application Code"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${LOG_FILE}
-if [ $? -eq 0 ]; then
-  echo Status = Success
-else
-  echo Status = Failure
-  exit 1
-fi
+StatusCheck $?
 
 cd /home/roboshop
 
 echo "Clean Old App Content"
 rm -rf catalogue &>>${LOG_FILE}
-if [ $? -eq 0 ]; then
-  echo Status = Success
-else
-  echo Status = Failure
-  exit 1
-fi
+StatusCheck $?
 
 echo "Extracting Catalogue Application Code"
 unzip /tmp/catalogue.zip &>>${LOG_FILE}
-if [ $? -eq 0 ]; then
-  echo Status = Success
-else
-  echo Status = Failure
-  exit 1
-fi
+StatusCheck $?
 
 mv catalogue-main catalogue
 cd /home/roboshop/catalogue
 
 echo "Install NodeJS Dependencies"
 npm install &>>${LOG_FILE}
-if [ $? -eq 0 ]; then
-  echo Status = Success
-else
-  echo Status = Failure
-  exit 1
-fi
+StatusCheck $?
 
 echo "Setup catalogue Service"
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>${LOG_FILE}
-if [ $? -eq 0 ]; then
-  echo Status = Success
-else
-  echo Status = Failure
-  exit 1
-fi
+
 
 
 systemctl daemon-reload &>>${LOG_FILE}
@@ -93,9 +60,3 @@ systemctl enable catalogue &>>${LOG_FILE}
 
 echo "Start Cataloge Service"
 systemctl start catalogue &>>${LOG_FILE}
-if [ $? -eq 0 ]; then
-  echo Status = Success
-else
-  echo Status = Failure
-  exit 1
-fi
