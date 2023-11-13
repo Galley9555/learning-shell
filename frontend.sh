5057d0905c7ed88d4cd4eb95c0ef18055746064b
OM SAI RAM JI <ricxrocxx1988@gmail.com>
2023-11-14 00:05:26 +0530

    Adding New Functionality

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,29 @@
-yum install nginx -y
-systemctl enable nginx
-systemctl start nginx
+LOG_FILE=/tmp/catalogue
+
+source Common.sh
+
+echo Installing Nginx
+yum install nginx -y &>>$LOG_FILE
+StatusCheck $?
+
+echo Downloading Nginx Web Content
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+StatusCheck $?
+
 cd /usr/share/nginx/html
-rm -rf *
-unzip /tmp/frontend.zip
-mv frontend-main/static/* .
-mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
-systemctl restart nginx
+
+echo Removing Old Web Content
+rm -rf * &>>/tmp/frontend
+StatusCheck $?
+
+echo Extracting Web Content
+unzip /tmp/frontend.zip &>>$LOG_FILE
+StatusCheck $?
+
+mv frontend-main/static/* . &>>$LOG_FILE
+mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
+
+echo Starting Nginx Service
+systemctl enable nginx &>>$LOG_FILE
+systemctl restart nginx &>>$LOG_FILE
+StatusCheck $?
--- mongodb.sh
+++ mongodb.sh
@@ -4,37 +4,37 @@ source Common.sh
 
 echo "Setting MongoDB Repo"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG_FILE
-echo status = $?
+StatusCheck $?
 
 echo "Installing MongoDB Server"
 yum install -y mongodb-org
-echo status = $?
+StatusCheck $?
 
 echo "Update MongoDB Listen Address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-echo status = $?
+StatusCheck $?
 
 echo "Starting MongoDB Service"
 systemctl enable mongod &>>$LOG_FILE
 systemctl restart mongod &>>$LOG_FILE
-echo status = $?
+StatusCheck $?
 
 echo "Downloading MongoDB Schema"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG_FILE
-echo status = $?
+StatusCheck $?
 
 cd /tmp
 
 echo "Extracting Schema Files"
 unzip mongodb.zip &>>$LOG_FILE
-echo status = $?
+StatusCheck $?
 
 cd mongodb-main
 
 echo "Load Catologue Service Schema"
 mongo < catalogue.js &>>$LOG_FILE
-echo status = $?
+StatusCheck $?
 
 echo "Load User Service Schema"
 mongo < users.js &>>$LOG_FILE
-echo status = $?
+StatusCheck $?
