5d09edb2e96ec942f46604262c310858e67d2454
OM SAI RAM JI <ricxrocxx1988@gmail.com>
2023-11-20 12:42:21 +0530

    Adding New Functionality

--- Redis.sh
+++ Redis.sh
@@ -1,21 +1,21 @@
-LOG_FILE=/tmp/catalogue
+LOG_FILE=/tmp/redis
 
 source Common.sh
 
 echo "Setup YUM Repos for Redis"
-dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>LOG_FILE
 StatusCheck $?
 
 echo " Enabling Redis YUM Modules"
-dnf module enable redis:remi-6.2 -y
+dnf module enable redis:remi-6.2 -y &>>LOG_FILE
 StatusCheck $?
 
 echo "Installing Redis"
 yum install redis -y &>>$LOG_FILE
 StatusCheck $?
 
-echo "Update Redis Listen Address from 127:0:0:1 to 0:0:0:0"
-sed -i -e 's/127:0:0:1/0:0:0:0' /etc/redis.conf /etc/redis/redis.conf
+echo "Update Redis Listen Address from 127.0.0.1 to 0.0.0.0"
+sed -i -e 's/127.0.0.1/0.0.0.0' /etc/redis.conf /etc/redis/redis.conf
 StatusCheck $?
 
 
