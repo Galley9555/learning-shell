e1dfc4f6e08d531930b5641140bef634bebf6a77
OM SAI RAM JI <ricxrocxx1988@gmail.com>
2023-11-27 10:25:03 +0530

    Adding New Functionality

--- Cart.sh
+++ Cart.sh
@@ -1,9 +1,5 @@
-# curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-# yum install nodejs -y
-# useradd roboshop
-$ curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
-$ cd /home/roboshop
-$ unzip /tmp/cart.zip
-$ mv cart-main cart
-$ cd cart
-$ npm install
+COMPONENT=cart
+LOG_FILE=/tmp/${COMPONENT}
+
+source common.sh
+NODEJS
