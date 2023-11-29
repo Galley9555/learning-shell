d7f555e909cf1703a9821eaf3512190ff97101c1
OM SAI RAM JI <ricxrocxx1988@gmail.com>
2023-11-29 16:00:14 +0530

    Adding New Functionality

--- Mysql.sh
+++ Mysql.sh
@@ -1,5 +1,5 @@
 LOG_FILE=/tmp/mysql
-source common.sh
+source Common.sh
 
 echo "Setting Up MySQL Repo"
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>$LOG_FILE
